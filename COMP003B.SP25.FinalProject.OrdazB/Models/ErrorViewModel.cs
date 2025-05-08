efbb9ce4f3e12fd411fa56b2069d6655e546de13
briannaordaz <briannaordaz5@gmail.com>
2025-05-07 19:10:32 -0700

    Installed Nugget Packages

---
+++ COMP003B.SP25.FinalProject.OrdazB/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace COMP003B.SP25.FinalProject.OrdazB.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
